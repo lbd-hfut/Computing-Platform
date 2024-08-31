1f8683a7298a5d2a4ba35b453558c73c447eba09
lbd-hfut <2862275482@qq.com>
2024-08-31 22:08:42 +0800

    add demos

---
+++ demos/config.py
@@ -0,0 +1,23 @@
+config = {
+    "checkpoint_path": "./weights/checkpoint/",
+    "model_path": "./weights/models/",
+    "data_path": './data/train/',
+    "warm_lr": 0.001,
+    "train_lr": 0.0005,
+    "max_iter": 20,
+    "weight_decay": 3e-2,
+    "layers": [2, 50, 50, 50, 2],
+    "scale": [[1,1]],
+    "warm_adam_epoch": 10,
+    "warm_bfgs_epoch": 21,
+    "train_adam_epoch": 10,
+    "train_bfgs_epoch": 21,
+    "patience_adam": 10,
+    "patience_lbfgs": 10,
+    "delta_warm_adam": 0.01,
+    "delta_warm_lbfgs": 0.005,
+    "delta_train_adam": 0.003,
+    "delta_train_lbfgs": 0.001,
+    "epoch": 0,
+    "print_feq": 10
+}
